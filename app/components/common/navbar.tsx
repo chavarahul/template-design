cc18df6d417dd4ac1491d91a3d223530cda93e6f
chavarahul <chavarahul7@gmail.com>
2024-10-31 11:05:00 +0530

    updatednavbar

--- app/components/common/navbar.tsx
+++ app/components/common/navbar.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
-// import { ThemeToggle } from '../ui/index'
+import { ThemeToggle } from '../ui/index'
 
 const Navbar = () => {
   return (
-    <header className='border p-3 h-[11vh]'>
-
+    <header className='borders p-3 h-[11vh]'>
+      <div className="flex justify-between w-full h-full borders px-10">
+        <h1 className='font-semibold text-2xl flex items-center'>
+          LogoName
+        </h1>
+        <nav className="borders w-1/5 h-full relative flex items-center justify-evenly">
+          <ThemeToggle />
+        </nav>
+      </div>
     </header>
   )
 }
--- app/components/ui/index.ts
+++ app/components/ui/index.ts
@@ -2,10 +2,12 @@ import { ThemeToggle } from "./theme-toggle";
 import { Button } from "./button";
 import { DropdownMenu } from "./dropdown-menu";
 import { ThemeProvider } from "./theme-provider";
+import { Dialog } from "./dialog";
 
 export{
     ThemeToggle,
     ThemeProvider,
     Button,
-    DropdownMenu
+    DropdownMenu,
+    Dialog
 }
